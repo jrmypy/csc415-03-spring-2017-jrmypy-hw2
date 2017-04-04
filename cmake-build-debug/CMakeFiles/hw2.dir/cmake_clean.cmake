611e58c16037bed9dca3a33053ade61d5f407c21
jrmypy <jrmypy@gmail.com>
2017-04-04 10:00:02 +0000

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/hw2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2.dir/src/main.c.o"
+  "CMakeFiles/hw2.dir/src/shell.c.o"
+  "CMakeFiles/hw2.dir/src/Tests/test.c.o"
+  "hw2.pdb"
+  "hw2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
